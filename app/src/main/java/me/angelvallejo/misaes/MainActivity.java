43d842f3678ec5f2735bf848d96db3bc8e7ff2a2
Angel <a.vallejoch@gmail.com>
2020-05-29 00:11:51 -0500

    Adding progressBar

--- app/src/main/java/me/angelvallejo/misaes/MainActivity.java
+++ app/src/main/java/me/angelvallejo/misaes/MainActivity.java
@@ -5,6 +5,7 @@ import me.angelvallejo.misaes.databinding.ActivityMainBinding;
 
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
                           implements GetContent.GetContentListener{
@@ -24,11 +25,14 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onResume() {
         super.onResume();
+        binding.progressBar.setVisibility(View.VISIBLE);
         new GetContent(this).execute(GetContent.Action.LOAD_LOGIN);
     }
 
     @Override
     public <T> void onResultReady(GetContent.Action action, T result) {
+        if (binding.progressBar.getVisibility() == View.VISIBLE)
+            binding.progressBar.setVisibility(View.INVISIBLE);
         switch (action){
             case LOAD_LOGIN:
                 byte[] captchaResponse = (byte[]) result;
