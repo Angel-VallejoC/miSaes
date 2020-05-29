c0a38fc9350e9fe74cfdca46708c137999b6d6cb
Angel <a.vallejoch@gmail.com>
2020-05-28 23:51:14 -0500

    Adding functionality to load login page an displaying captcha image

--- app/src/main/java/me/angelvallejo/misaes/MainActivity.java
+++ app/src/main/java/me/angelvallejo/misaes/MainActivity.java
@@ -3,9 +3,11 @@ package me.angelvallejo.misaes;
 import androidx.appcompat.app.AppCompatActivity;
 import me.angelvallejo.misaes.databinding.ActivityMainBinding;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+                          implements GetContent.GetContentListener{
     private static final String TAG = "MainActivity";
 
     private ActivityMainBinding binding;
@@ -17,5 +19,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new GetContent(this).execute(GetContent.Action.LOAD_LOGIN);
+    }
+
+    @Override
+    public <T> void onResultReady(GetContent.Action action, T result) {
+        switch (action){
+            case LOAD_LOGIN:
+                byte[] captchaResponse = (byte[]) result;
+                binding.captchaImage.setImageBitmap(
+                        BitmapFactory.decodeByteArray(
+                                captchaResponse,0,captchaResponse.length
+                        )
+                );
+                return;
+        }
     }
 }
