313a0c0780bb25f6bc4f22473d9da74346cdb27d
Angel <a.vallejoch@gmail.com>
2021-02-06 21:47:17 -0600

    Adding a menu to home screen, implementing logout

--- app/src/main/java/me/angelvc/misaes/home/HomeActivity.java
+++ app/src/main/java/me/angelvc/misaes/home/HomeActivity.java
@@ -1,7 +1,11 @@
 package me.angelvc.misaes.home;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
@@ -9,6 +13,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import me.angelvc.misaes.R;
 import me.angelvc.misaes.databinding.ActivityHomeBinding;
+import me.angelvc.misaes.login.LoginActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -40,4 +45,35 @@ public class HomeActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_logout){
+            showLogoutDialog();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showLogoutDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder( this, R.style.CustomDialogTheme);
+        builder.setTitle("Cerrar sesión")
+                .setMessage("¿Está seguro que desea cerrar sesión?")
+                .setPositiveButton("Cerrar sesión", (dialogInterface, i) -> {
+                    logout();
+                })
+                .setNegativeButton("Cancelar", ((dialogInterface, i) -> { }))
+        ;
+        builder.create().show();
+    }
+
+    private void logout() {
+        startActivity(new Intent(this, LoginActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
 }
---
+++ app/src/main/java/me/angelvc/misaes/home/HomeContracts.java
@@ -0,0 +1,10 @@
+package me.angelvc.misaes.home;
+
+public interface HomeContracts {
+    interface View {
+        void showLogoutDialog();
+        void logout();
+    }
+
+
+}
